f9c1f2108d35c9d733e828e429e22769acf59ffd
jc9361 <jc9361i@gmail.com>
2020-12-06 15:49:51 -0800

    updating repo...

--- env/scripts/surf/searcher.py
+++ env/scripts/surf/searcher.py
@@ -5,9 +5,9 @@ def searcher():
     os.system('clear')
     print("                         _")
     print(" ___  ___  __ _ _ __ ___| |__")
-    print("/ __|/ _ \/ _` | '__/ __| '_ \ ")
-    print("\__ \  __/ (_| | | | (__| | | |")
-    print("|___/\___|\__,_|_|  \___|_| |_|")
+    print("/ __|/ _ \\/ _` | '__/ __| '_ \\ ")
+    print("\\__ \\  __/ (_| | | | (__| | | |")
+    print("|___/\\___|\\__,_|_|  \\___|_| |_|")
     print("\n")
     
     usr_input = input("> ")
@@ -15,11 +15,12 @@ def searcher():
     if usr_input != 'q':
         #search_string = f"surf 'google.com/search?&q={usr_input}'"
         search_string = f"surf 'duckduckgo.com/{usr_input}' &"
-        search = subprocess.call(search_string, shell=True)
+        subprocess.call(search_string, shell=True)
         searcher()
     else:
         os.system('clear')
         quit()
+
 
 
 searcher()
--- env/touchy.sh
+++ env/touchy.sh
@@ -8,12 +8,15 @@
 # Bash script for setting up touchpad/trackpoint for BSPWM
 
 
-# MACBOOK PRO
+#===[ MACBOOK PRO ]===#
 
 # TOUCHPAD
 xinput set-prop bcm5974 libinput\ Tapping\ Enabled 1
 xinput set-prop bcm5974 libinput\ Natural\ Scrolling\ Enabled 1
 xinput set-prop bcm5974 libinput\ Accel\ Speed 0.5
+
+
+#===[ THINKPAD X220 ]===#
 
 # TRACKPAD ON/OFF (0=off/1=on)
 # xinput set-prop SynPS/2\ Synaptics\ TouchPad "Device Enabled" 0
