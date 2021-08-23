fb82e34a8ee67402b13b1dcebcae5f70ed3f2700
jc9361 <jc9361i@gmail.com>
2021-08-23 02:23:13 -0700

    adding version numbers to applications

--- env/scripts/paclist.sh
+++ env/scripts/paclist.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# WITHOUT VERSION NUMBER
+# pacman --color=auto -Q | awk '{print $1}' | less
 
-pacman --color=auto -Q | awk '{print $1}' | less
+
+# WITH VERSION NUMBER
+pacman --color=auto -Q | less
