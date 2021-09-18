2e91b6a187c9a9f9ee25b20100557265af621f4e
jc9361 <jc9361i@gmail.com>
2021-09-17 20:54:08 -0700

    adding rofi_youtube_search

---
+++ env/scripts/rofi/rofi_youtube_search.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BROWSER=surf
+# read selection <<< $(rofi -dmenu -p "SEARCH" -font "Hack Nerd font 10" -location 1 -width 20 -lines 100 -m 1)
+read selection <<< $(rofi -dmenu -p "SEARCH YOUTUBE" -font "Hack Nerd font 10" \
+    -location 0 -width 25% -lines 10 -theme-str '#window { width:25%; height:25%; }')
+
+
+
+if [[ $selection == "" ]]; then
+    echo nothing selected, exiting...
+    sleep 1
+    clear
+else
+    """ Query string """
+    # echo $BROWSER "youtube.com/search?&q=""$selection"
+    $BROWSER "youtube.com/search?&q=""$selection"
+fi
